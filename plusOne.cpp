4b5aa868e347f6ab2b70a216008af2be4ddf073f
Noa Kirschbaum <noakirschbaum@gmail.com>
2025-07-29 22:07:40 -0500

    new solution

--- plusOne.cpp
+++ plusOne.cpp
@@ -3,49 +3,28 @@
 
 using namespace std;
 
-bool is_special_case(vector<int>& digits){
-    
-    int counter = 0;
-    for (int num : digits){
-        if (num == 9){
-            counter++;
-        }
-    }
-    if (counter == digits.size()){
-        return true;
-    } 
-    else {
-        return false;
-    }
-}
 
 vector<int> plusOne(vector<int>& digits){
     
-    bool all_nines = is_special_case(digits); // find special case ex: 999
-
-    int last_digit = digits.back(); // get last element's value
-    
-    digits.pop_back();
-    
-    if (all_nines){
-        vector<int> new_digits {1, 0};
-        for (int nine : digits){
-            new_digits.push_back(0);
-        }
-        return new_digits;
-    } 
-    else { 
-        
-        if (last_digit == 9){
-            digits.insert(digits.end() - 1, digits.back() + 1);
-            digits.push_back(0);
-            return digits;
+    bool push_one = false;
+    int num = digits.size()-1;
+    for (num; num > 0; --num){
+        if (digits[num] == 9){
+            digits[num] = 0;
+            push_one = true;
         }
         else {
-            digits.push_back(last_digit + 1);
-            return digits;
+            digits[num] += 1;
+            break;
         }
-    }    
+    }
+
+    if(push_one){
+        digits[num] += 1;
+    }
+
+    return digits;
+
 }
 
 void print_number(vector<int>& vec, int inout){
@@ -55,7 +34,7 @@ void print_number(vector<int>& vec, int inout){
     }
     cout << "\n";
     
-    if (inout = 0){
+    if (inout == 0){
     cout << "last_digit: " << vec.back() << "\n";
     }
 }
@@ -79,7 +58,7 @@ int main(){
     print_number(output_v, 1);
     cout << "\n";
 
-    input_v = {9,9,8};
+    input_v = {8,9,9};
     print_number(input_v, 0);
 
     output_v = plusOne(input_v);
